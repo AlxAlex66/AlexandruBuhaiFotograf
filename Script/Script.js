3b975ed1110b64b946a94b7f38961aea8eee4b8f
Alexandru Buhai <alxalex66@yahoo.com>
2024-01-14 15:55:42 +0200

    Coiere Site Mama

---
+++ Script/Script.js
@@ -0,0 +1,45 @@
+function navSlide(){
+  //elemente selectate
+  const butonMeniu = document.querySelector('.buton-mobil');
+  const navigatie = document.querySelector('.lista-navigare');
+  const paginiMeniu = document.querySelectorAll('.lista-navigare li')
+
+      butonMeniu.addEventListener('click', function() {
+      // iesire bara de navigare
+      navigatie.classList.toggle('lista-navigare-activare');
+
+      // Animare butoanele ce ne trimit la pagini
+      paginiMeniu.forEach((link, index ) => {
+        if(link.style.animation)
+        { link.style.animation = '';
+      }else {
+
+
+          link.style.animation = `miscareMeniu 0.5s ease forwards ${index/7+0.3}s `  //folosim oentru animarea fiecarui element din bara de navigatie,cu o anumita intarziere intre ele
+        }
+
+       //link.style.animation=`miscareMeniu 0.5 ease forwards $(index /7 + 0.3)
+
+
+   });
+   // animatie buton de MENIU
+   butonMeniu.classList.toggle('transformare-buton-meniu');
+
+ });
+
+
+
+}
+const trimitereSus = document.querySelector('.mergiSus');
+  window.addEventListener('scroll', () => {
+    if(window.pageYOffset > 100) {
+      trimitereSus.classList.add('active');
+    }else
+    {
+      trimitereSus.classList.remove('active');
+    }
+})
+
+
+
+navSlide();
