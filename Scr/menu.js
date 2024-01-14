430bc459669d597fccbebdc4a7761895be65309f
Alexandru Buhai <alxalex66@yahoo.com>
2024-01-14 16:25:34 +0200

    actualizare nume

--- Scr/menu.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('menu').addEventListener('click', function(event) {
-    event.preventDefault();
-    var target = event.target;
-    if (target.tagName === 'A') {
-        window.location.hash = target.hash;
-    }
-});
